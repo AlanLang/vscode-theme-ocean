6ec8777e49a2ccb628f2b982dc4528dffa788574
meimei <862955678@qq.com>
2019-03-27 11:42:46 +0800

    feat(1): 2
    3
    
    BREAKING CHANGE: 4
    Closes 5

--- src/commitizen.js
+++ src/commitizen.js
@@ -1,4 +1,130 @@
 const vscode = require('vscode');
-const commitizen = vscode.commands.registerCommand('extension.commitizen', () => {
 
-})
+const DEFAULT_TYPES = [
+  {
+    label: 'feat',
+    description: '新增功能'
+  },
+  {
+    label: 'fix',
+    description: '修复bug'
+  },
+  {
+    label: 'docs',
+    description: '修改文档'
+  },
+  {
+    label: 'style',
+    description: '调整代码格式, 不改变代码逻辑 (空格, 布局, 缩进, 等等...)'
+  },
+  {
+    label: 'refactor',
+    description: '代码重构，未新增任何功能和修复任何bug'
+  },
+  {
+    label: 'perf',
+    description: '改善性能和体现'
+  },
+  {
+    label: 'test',
+    description: '增加或修改测试用例'
+  },
+  {
+    label: 'build',
+    description: '改变构建流程，新增依赖库、工具等'
+  },
+  {
+    label: 'ci',
+    description: '自动化流程配置修改'
+  },
+  {
+    label: 'chore',
+    description: '非src和test的修改'
+  },
+  {
+    label: 'revert',
+    description: '回滚到上一个版本'
+  }
+];
+
+const DEFAULT_MESSAGES = {
+  type: '请选择提交的类型',
+  scope: '说明commit的影响范围 [可空]',
+  subject: '请简短清晰得描述本次提交的内容',
+  body: '对本次提交的详细描述，使用 "|" 换行 [可空]',
+  breaking: '列出所有破坏性的变动 [可空]',
+  footer: '列出本次提交关闭的issues E.g.: #31, #34 [可空]'
+};
+
+const COMMIT_VALUE = {
+  type: '',
+  scope: '',
+  subject: '',
+  body: '',
+  breaking: '',
+  footer: ''
+};
+
+const commitizen = vscode.commands.registerCommand('extension.commitizen', () => {
+  vscode.window.showQuickPick(DEFAULT_TYPES, getOption(DEFAULT_MESSAGES.type))
+  .then(command => {
+    COMMIT_VALUE.type = command;
+    const options = {
+      placeHolder: DEFAULT_MESSAGES.scope,
+      ignoreFocusOut: true
+    };
+    return vscode.window.showInputBox(options)
+  }).then(command => {
+    COMMIT_VALUE.scope = command;
+    const options = {
+      placeHolder: DEFAULT_MESSAGES.subject,
+      ignoreFocusOut: true,
+      validateInput : input => {
+        const maxLenght = 50;
+        if (input.length === 0 || input.length > maxLenght) {
+          return `提交内容为必填且必须小于50个字符`;
+        }
+        return '';
+      }
+    };
+    return vscode.window.showInputBox(options)
+  }).then(command => {
+    COMMIT_VALUE.subject = command;
+    const options = {
+      placeHolder: DEFAULT_MESSAGES.body,
+      ignoreFocusOut: true
+    };
+    return vscode.window.showInputBox(options)
+  }).then(command => {
+    COMMIT_VALUE.body = command;
+    const options = {
+      placeHolder: DEFAULT_MESSAGES.breaking,
+      ignoreFocusOut: true
+    };
+    return vscode.window.showInputBox(options)
+  }).then(command => {
+    COMMIT_VALUE.breaking = command;
+    const options = {
+      placeHolder: DEFAULT_MESSAGES.footer,
+      ignoreFocusOut: true
+    };
+    return vscode.window.showInputBox(options)
+  }).then(command => {
+    COMMIT_VALUE.footer = command;
+    const channel = vscode.window.createOutputChannel('commitizen');
+    channel.appendLine('Commitizen support started');
+    channel.appendLine(`About to commit `);
+    channel.show();
+  })
+});
+
+function getOption(msg){
+  return {
+    placeHolder: msg,
+    ignoreFocusOut: true,
+    matchOnDescription: true,
+    matchOnDetail: true
+  }
+}
+
+module.exports = commitizen;
